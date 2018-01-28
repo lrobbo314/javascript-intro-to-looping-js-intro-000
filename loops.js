e01d23f79ee801f3b2e18ad54e727c24edf7a3e2
Robert Lego <robertlego@gmail.com>
2018-01-28 00:50:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -28,6 +28,6 @@ function doWhileLoop(array) {
     array.pop();
     console.log(array.length);
   }
-  while (array.length > 0 || maybeTrue()===false);
+  while (array.length > 0 || maybeTrue() === false);
   return array;
 }
